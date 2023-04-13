f4af06ead10a362de6c7fcc2030fca45ccd216b6
dark fox <danyamatlaga63@gmail.com>
2023-04-13 10:57:37 +0300

    Добавьте файлы проекта.

---
+++ ClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using ClientWPF.Pages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Teacher_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            LoginWindow login = new LoginWindow();
+            login.Owner = this;
+            login.ShowDialog();
+            this.Close();
+        }
+
+        private void Student_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+            LoginWindow login = new LoginWindow();
+            login.Owner = this;
+            login.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ ClientWPF/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace ClientWPF.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        int it = 0;
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Registr_btn_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow registr = new RegisterWindow();
+            registr.Owner = this;
+            this.Visibility = Visibility.Hidden;
+            registr.ShowDialog();
+            this.Close();
+        }
+
+        private void Username_txb_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            if (it == 0)
+            {
+                Username_txb.Text = "";
+                it++;
+            }
+        }
+    }
+}
